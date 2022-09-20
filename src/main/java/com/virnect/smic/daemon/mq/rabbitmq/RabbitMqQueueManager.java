0734e262772160768882c7611d45df6fc1e5d6c2
sangeun oh <sangeun@virnect.com>
2022-09-20 10:59:19 +0900

    .

--- src/main/java/com/virnect/smic/daemon/mq/rabbitmq/RabbitMqQueueManager.java
+++ src/main/java/com/virnect/smic/daemon/mq/rabbitmq/RabbitMqQueueManager.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeoutException;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
