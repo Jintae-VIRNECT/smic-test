732eaab0a2afbeda0ddb2d35b8cb801cc00c1dbe
sangeun oh <sangeun@virnect.com>
2022-09-20 09:05:52 +0900

    .

--- src/main/java/com/virnect/smic/common/data/domain/Device.java
+++ src/main/java/com/virnect/smic/common/data/domain/Device.java
@@ -1,6 +1,5 @@
 package com.virnect.smic.common.data.domain;
 
-import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -11,7 +10,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
